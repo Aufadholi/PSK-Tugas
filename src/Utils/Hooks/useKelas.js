fc4f56fbb79c9578b6d27da513a466ea552b0aea
Aufadholi <aufa.fadholi@gmail.com>
2025-12-11 08:23:09 +0700

    feat: refactor useKelas and useMataKuliah hooks to utilize API functions and improve data management

--- src/Utils/Hooks/useKelas.js
+++ src/Utils/Hooks/useKelas.js
@@ -1,62 +1,45 @@
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/kelas';
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  getAllKelas,
+  storeKelas,
+  updateKelas,
+  deleteKelas,
+} from "@/Utils/Apis/KelasApi";
 
 export function useKelas() {
   const queryClient = useQueryClient();
 
-  const {
-    data: kelas,
-    isLoading,
-    error,
-  } = useQuery(['kelas'], async () => {
-    const res = await axios.get(API_URL);
-    return res.data;
+  const kelasQuery = useQuery({
+    queryKey: ["kelas"],
+    queryFn: getAllKelas,
   });
 
-  const createKelas = useMutation(
-    async (newData) => {
-      const res = await axios.post(API_URL, newData);
-      return res.data;
+  const createKelas = useMutation({
+    mutationFn: storeKelas,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["kelas"] });
     },
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries(['kelas']);
-      },
-    }
-  );
+  });
 
-  const updateKelas = useMutation(
-    async ({ id, ...updateData }) => {
-      const res = await axios.put(`${API_URL}/${id}`, updateData);
-      return res.data;
+  const updateKelasMutation = useMutation({
+    mutationFn: ({ id, data }) => updateKelas(id, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["kelas"] });
     },
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries(['kelas']);
-      },
-    }
-  );
+  });
 
-  const deleteKelas = useMutation(
-    async (id) => {
-      await axios.delete(`${API_URL}/${id}`);
-      return id;
+  const deleteKelasMutation = useMutation({
+    mutationFn: deleteKelas,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["kelas"] });
     },
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries(['kelas']);
-      },
-    }
-  );
+  });
 
   return {
-    kelas,
-    isLoading,
-    error,
+    kelasQuery,
     createKelas,
-    updateKelas,
-    deleteKelas,
+    updateKelasMutation,
+    deleteKelasMutation,
   };
 }
--- src/Utils/Hooks/useMatakuliah.js
+++ src/Utils/Hooks/useMatakuliah.js
@@ -1,62 +1,45 @@
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/matakuliah';
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  getAllMataKuliah,
+  storeMataKuliah,
+  updateMataKuliah,
+  deleteMataKuliah,
+} from "@/Utils/Apis/MataKuliahApi";
 
-export function useMatakuliah() {
+export function useMataKuliah() {
   const queryClient = useQueryClient();
 
-  const {
-    data: matakuliah,
-    isLoading,
-    error,
-  } = useQuery(['matakuliah'], async () => {
-    const res = await axios.get(API_URL);
-    return res.data;
+  const mataKuliahQuery = useQuery({
+    queryKey: ["matakuliah"],
+    queryFn: getAllMataKuliah,
   });
 
-  const createMatakuliah = useMutation(
-    async (newData) => {
-      const res = await axios.post(API_URL, newData);
-      return res.data;
+  const createMataKuliah = useMutation({
+    mutationFn: storeMataKuliah,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["matakuliah"] });
     },
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries(['matakuliah']);
-      },
-    }
-  );
+  });
 
-  const updateMatakuliah = useMutation(
-    async ({ id, ...updateData }) => {
-      const res = await axios.put(`${API_URL}/${id}`, updateData);
-      return res.data;
+  const updateMataKuliahMutation = useMutation({
+    mutationFn: ({ id, data }) => updateMataKuliah(id, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["matakuliah"] });
     },
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries(['matakuliah']);
-      },
-    }
-  );
+  });
 
-  const deleteMatakuliah = useMutation(
-    async (id) => {
-      await axios.delete(`${API_URL}/${id}`);
-      return id;
+  const deleteMataKuliahMutation = useMutation({
+    mutationFn: deleteMataKuliah,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["matakuliah"] });
     },
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries(['matakuliah']);
-      },
-    }
-  );
+  });
 
   return {
-    matakuliah,
-    isLoading,
-    error,
-    createMatakuliah,
-    updateMatakuliah,
-    deleteMatakuliah,
+    mataKuliahQuery,
+    createMataKuliah,
+    updateMataKuliahMutation,
+    deleteMataKuliahMutation,
   };
 }
